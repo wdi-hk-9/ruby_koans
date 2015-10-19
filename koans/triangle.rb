a901d039ca4acc83864dc425c6d7c13d1d9a64c2
Philippa Johnstone <pecj@hotmail.co.uk>
2015-10-20 00:54:54 +0800

    triangle.rb completed

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,7 +15,15 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b && b == c then
+    :equilateral
+  elsif a == c || a == b || b == c then
+    :isosceles
+  else
+    :scalene
+  end
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
