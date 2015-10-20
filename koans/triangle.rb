4ba589ae29aed69c23c34143fe48c411bb3eb9ac
ianchoo <ianienchoo@gmail.com>
2015-10-20 21:17:47 +0800

    koans up to 179 done

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  x,y,z = [a,b,c].sort
+  raise TriangleError if x <= 0 || x + y <= z
+
+  [:equilateral,:isosceles,:scalene].fetch([a,b,c].uniq.length - 1)
 end
 
 # Error class used in part 2.  No need to change this code.
