a09fb08375e57e51a445800f2d0a4943aa9f7aad
Philippa Johnstone <pecj@hotmail.co.uk>
2015-10-21 20:58:10 +0800

    about_proxy_onjects_project completed

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -12,13 +12,37 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # missing handler and any other supporting methods.  The specification
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
+
 class Proxy
+
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def called?(method_name)
+    @messages.include? method_name
+  end
+
+  def number_of_times_called(method_name)
+    @messages.count method_name
+  end
+
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to? method_name then
+      # track each method called that target object can respond to
+      @messages.push method_name
+
+      # call the method!
+      @object.send method_name, *args
+    else
+      # all other cases: default behavior (raises NoMethodError)
+      super method_name, *args, &block
+    end
+  end
+
 end
 
 # The proxy object should pass the following Koan:
